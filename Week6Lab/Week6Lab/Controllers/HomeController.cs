acd1baeaab4c7c6c94eda2e7c831972a6c973fdd
Мариям Сисенгали <msisengali@gmail.com>
2018-02-14 16:12:54 +0600

    week6lab

---
+++ Week6Lab/Week6Lab/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Week6Lab.Models;
+
+namespace Week6Lab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Calc calc) {
+            ModelState.Clear();
+            switch (calc.operationType)
+            {
+                case Operation.Addition:
+                    calc.Result = calc.Number1 + calc.Number2;
+                    break;
+                case Operation.Substraction:
+                    calc.Result = calc.Number1 - calc.Number2;
+                    break;
+                case Operation.Multiplication:
+                    calc.Result = calc.Number1 * calc.Number2;
+                    break;
+                case Operation.Division:
+                    calc.Result = calc.Number1 / calc.Number2;
+                    break;
+
+
+            }
+
+            return View(calc);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Week6Lab/Week6Lab/Models/Calc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Week6Lab.Models
+{
+    public enum Operation
+    {
+        Addition = '+',
+        Substraction = '-',
+        Multiplication = '*',
+        Division = '/'
+    }
+    public class Calc
+    {
+        public decimal Number1 { get; set; }
+        public decimal Number2 { get; set; }
+        public Operation operationType { get; set; }
+        public decimal Result { get; set; }
+    }
+}
